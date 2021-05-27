6d6ba2041d2fe10e9f214aaed11bfaebb9d276d4
danthogi <srivani.danthogi@gmail.com>
2021-05-27 20:34:25 +0530

    steps to follow for checking in

---
+++ client/stepsToFollow.ts
@@ -0,0 +1,5 @@
+// step 1, clone the way by taking from your own branch and not master
+// step 2, add it to git url by giving in ctrl + shift + p
+// step 3, make sure that you are in your own branch by checking bottom left most
+// step 4, if all the above 3 are satisfied, you are good to go and make code changes
+// 
